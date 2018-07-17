e2186a15c082d210fb4b753e4d99a626f99510fc
LIGANG08 <197346468@qq.com>
2018-07-17 18:14:56 +0800

    angular01（英雄指南创建服务，待续）

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero/hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,11 +13,15 @@ export class HeroesComponent implements OnInit {
   //   id: 1,
   //   name: 'windstorm'
   // }
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() { }
-
+  constructor( private heroService: HeroService ) { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   ngOnInit() {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
